a7f95c6389576d33dc14c5fe29f319d5530b054e
rfauziarif <muhammadrizkifauziarif@gmail.com>
2022-06-12 12:54:40 +0700

    feat:ini adalah commit pertamaku

---
+++ index.js
@@ -0,0 +1,8 @@
+const arr =[100, 200,300]; // deklarasi array secara langsung
+
+const arr2 = []; //inisialisasi array denga jumlah tidak diketahui (nambahinnya belakangan )
+
+arr2 [0] = 100;  
+arr2 [1]= 200 ; 
+
+console.log(arr);
---
+++ main.js
@@ -0,0 +1,5 @@
+const pi=3.14; 
+let nama= ("ini adalah namaku"); 
+
+console.log (nama); 
+console.log (pi);
